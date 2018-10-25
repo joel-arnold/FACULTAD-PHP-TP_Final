4b7707273023001b8189db65cb474806ecd1c779
Mailen <mailen.cardoso@yahoo.com.ar>
2018-10-25 15:43:58 -0300

    Actualizacion de conexion y Alta de personas

---
+++ altaPersonas.php
@@ -0,0 +1,35 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Alta Personas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+    <?php
+        include("conexion.php");
+
+        $nom_apel = $_POST['nom_apel'];
+        $email = $_POST['mail'];
+        $direc = $_POST['direccion'];
+        $telefono = $_POST['telefono'];
+        $fecha_nac = $_POST['fec_nac'];
+        $tipo_usu = $_POST['tipo_usuario'];
+
+        $vSQL = "insert into usuario (nombre y apellido, email, direccion, telefono, fecha_nacimiento, tipo_usuario")
+        values ('$nom_apel', '$email', '$direc', '$telefono','$fecha_nac', '$tipo_usu');
+
+        mysqli_query($link,$vSQL) or die(mysqli_error($link));
+
+        echo("Usuario registrado con exito");
+        echo();//poner hipervinculo para volver atras o redireccionar a pagina principal
+
+        mysqli_close($link);
+
+
+
+    ?>
+</body>
+</html>
--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("localhost","root");
+    $link = mysqli_connect("localhost","root") or die("Problemas de conexion a la base de datos");
 
     mysqli_select_db($link, "academia");
 
